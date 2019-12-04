f9e512e3cd0e424cdd2c98b8d22565e463028775
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-12-04 10:30:57 -0500

    used axios to call in data from github api, created files for user and follower cards

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+import UserCard from './components/UserCard';
 class App extends React.Component {
+  state = {
+    user: []
+  }
+
+  componentDidMount() {
+
+    axios.get('https://api.github.com/users/arianashackelford')
+    .then(response => {
+      console.log(response)
+      this.setState({ user: response.data });
+    });
+
+  }
   render(){
     return (
       <div className="App">
       <h1>Github User Cards</h1>
+      <UserCard user={user}/>
       </div>
     );
   }
